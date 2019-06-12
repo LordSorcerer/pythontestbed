92ea21bf1571ed9e1786b5da6cc02d1c5039e7c5
LordSorcerer <ms1@thousandstorms.com>
2019-06-12 14:22:15 -0400

    Chat Program

--- hibabe.py
+++ hibabe.py
@@ -1,25 +1,25 @@
-from flask import Flask
-
-# Imports the Google Cloud client library
-from google.cloud import translate
-
-# Instantiates a client
-translate_client = translate.Client()
-
-# The text to translate
-text = u'Hi, Babe!'
-# The target language
-target = 'ru'
-
-# Translates some text into Russian
-translation = translate_client.translate(
-    text,
-    target_language=target)
-
+from flask import Flask, request
+from jinja2 import Template
 
 app = Flask(__name__)
 
+with open('./html/index.html', 'r') as index:
+    defaultPage = index.read()
+
+renderPage = Template(defaultPage)
+chatText = "<h2>Welcome to the Chat!</h2>"
+
 @app.route("/")
 def hello():
-    return translation
+    return renderPage.render(chatText=chatText)
+    #defaultPage
+
+@app.route("/sendtext", methods=['POST'])
+def receiveText ():
+	newChatText = request.form.get('chattext') #if key doesn't exist, returns None
+	global chatText
+	chatText = chatText + "<p>" + newChatText + "</p>"
+	print (chatText)
+	return renderPage.render(chatText=chatText)
+
 
