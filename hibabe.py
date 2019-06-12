f9026e90a73566066783dec748b4e81a3c59dd8f
LordSorcerer <ms1@thousandstorms.com>
2019-06-12 12:15:19 -0400

    Heroku translate test

--- hibabe.py
+++ hibabe.py
@@ -1,7 +1,25 @@
 from flask import Flask
+
+# Imports the Google Cloud client library
+from google.cloud import translate
+
+# Instantiates a client
+translate_client = translate.Client()
+
+# The text to translate
+text = u'Hi, Babe!'
+# The target language
+target = 'ru'
+
+# Translates some text into Russian
+translation = translate_client.translate(
+    text,
+    target_language=target)
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi, babe!  Yes, this means you, Foxy."
+    return translation
 
