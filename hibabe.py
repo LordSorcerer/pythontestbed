a390034bd2b6347aa6fe08a714a9203a9172856e
LordSorcerer <ms1@thousandstorms.com>
2019-06-14 18:15:31 -0400

    NEW: CSS added to main page. FIXED: Reroutes to index after post.

--- hibabe.py
+++ hibabe.py
@@ -1,25 +1,38 @@
-from flask import Flask, request
+from flask import Flask, redirect, request, url_for
 from jinja2 import Template
+import time, schedule
 
 app = Flask(__name__)
 
-with open('./html/index.html', 'r') as index:
+with open('./static/html/index.html', 'r') as index:
     defaultPage = index.read()
 
 renderPage = Template(defaultPage)
-chatText = "<h2>Welcome to the Chat!</h2>"
-
-@app.route("/")
+chatText, chatTextBuffer = " ",""
+@app.route("/", methods=['GET'])
 def hello():
     return renderPage.render(chatText=chatText)
-    #defaultPage
 
-@app.route("/sendtext", methods=['POST'])
+@app.route("/sendmessage", methods=['POST'])
 def receiveText ():
-	newChatText = request.form.get('chattext') #if key doesn't exist, returns None
-	global chatText
-	chatText = chatText + "<p>" + newChatText + "</p>"
-	print (chatText)
+	global chatTextBuffer
+	newTime = time.strftime("%A, %d. %B %Y %I:%M:%S %p")
+	newUser = request.form.get('username')
+	newMessage = request.form.get('message') #if key doesn't exist, returns None
+	newChatText = "<div class='chatMessage'><span class='chatDate'>" + str(newTime) + "</span> <span class=chatText>" + newUser + ": " + newMessage + "</span></div>"
+	chatTextBuffer = chatTextBuffer + newChatText
+	print ("New message added to chatTextBuffer:\n ", newChatText)
+	updateChatText()
+	return redirect(url_for('hello'))
+
+def updateChatWindow():
 	return renderPage.render(chatText=chatText)
 
+def updateChatText ():
+	global chatText, chatTextBuffer
+	chatText = chatText + chatTextBuffer
+	chatTextBuffer = ""
+	print ("Updated chatText")
 
+schedule.every().second.do(updateChatWindow)
+schedule.run_pending()
